24ee052f9f40e8e1bc4b19f237dec10ba3212461
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-08 14:41:05 -0700

    prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - print "Programming is like building a multilingual puzzle
- *
- * Return: Always zero
+ * main - entry point
+ * Return: 0
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle")
-
-return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
