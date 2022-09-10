1fb3d3a11b7e1163063df63cc9129f63c768d5a0
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-09 19:35:10 -0700

    numbers in base 10 starting from zero

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all numbers of base 10, starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
