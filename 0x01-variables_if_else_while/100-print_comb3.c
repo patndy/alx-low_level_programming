c43d12076da5a6a509c313f8769c45ec8dc74d10
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-09 20:18:22 -0700

    print numbers from 0 to 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print numbers from 0 to 99.
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 100; i++)
+{
+j = i / 10;
+k = i % 10;
+putchar(j + '0');
+putchar(k + '0');
+if (i < 99)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
