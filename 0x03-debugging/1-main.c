2456b3bc5de90d9d9c8d57af9abc81df7c911663
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-16 12:34:38 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 	i = 0;
 	/**
 	 * while (i < 10)
@@ -17,6 +17,6 @@ int main(void)
 	 * }
 	 */
 
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
