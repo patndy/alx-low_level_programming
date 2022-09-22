7374bb7194e42430253a195a99ddcc685390f3ca
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-22 04:46:45 -0700

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,7 +8,7 @@ char *rot13(char *s)
 {
 int count = 0, i;
 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 52; i++)
