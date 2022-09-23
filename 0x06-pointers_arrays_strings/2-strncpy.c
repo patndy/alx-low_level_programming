c6a99ce7fd02281de27820dfb7115f337b124ead
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-09-23 07:51:05 -0700

    copies of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i;
-i = 0;
-while (i < n && src[i] != '\0')
-{
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
+for ( ; i < n; i++)
 dest[i] = '\0';
-i++;
-}
 return (dest);
 }
