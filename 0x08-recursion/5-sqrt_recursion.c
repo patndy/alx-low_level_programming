e259652fafb925e593897eb8957c2b5d257e82c5
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-10-05 04:18:10 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,19 +11,19 @@ if (n < 0)
 return (-1);
 else if (n == 0 || n == 1)
 return (n);
-return (isqrt(n, square));
+return (sqrt(n, square));
 }
 /**
- * isqrt - wrapper function to check for squaroot.
+ * sqrt - wrapper function to check for squaroot.
  * @n: number to check for squareroot.
  * @i: squareroot.
  * Return: 1 if squareroot is found, -1 if not found else recurse.
  */
-int isqrt(int n, int i)
+int sqrt(int n, int i)
 {
 if (i * i > n)
 return (-1);
 if (i * i == n)
 return (i);
-return (isqrt(n, i + 1));
+return (sqrt(n, i + 1));
 }
