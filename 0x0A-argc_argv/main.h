3dbed77adafb82f1815a2d3c10fea8b71e630219
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-10-06 03:34:22 -0700

    writes program name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+nt main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int isInteger(const char *s)
+int main(int argc, char const *argv[])
