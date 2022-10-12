2238451fb15cc3d2c0a3758bc582aabc4718efc9
Patrick Ndifreke <patrickndifreke@gmail.com>
2022-10-11 20:01:29 -0700

    multiplies two numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi_digit(char x);
+int _isNumber(char *argv);
+int _putchar(char c);
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
